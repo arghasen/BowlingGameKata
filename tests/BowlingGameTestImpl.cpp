d525f47e5484894ee132f4bad74260679262c2f1
Argha Sen <iamslowdeath@gmail.com>
2018-11-20 03:57:46 +0530

    refactor to remove duplicate game creation

--- tests/BowlingGameTestImpl.cpp
+++ tests/BowlingGameTestImpl.cpp
@@ -5,18 +5,21 @@
 #include "../ext/catch.h"
 #include "../src/Game.h"
 
-TEST_CASE("GutterGame"){
-    Game g;
-    for (int i = 0; i < 20; ++i) {
-        g.roll(0);
-    }
-    REQUIRE( g.score() == 0 );
-}
+TEST_CASE("BowlingGame") {
 
-TEST_CASE("Throw All Ones"){
     Game g;
-    for (int i = 0; i < 20; ++i) {
-        g.roll(1);
+
+    SECTION("Gutter Game") {
+        for (int i = 0; i < 20; ++i) {
+            g.roll(0);
+        }
+        REQUIRE(g.score() == 0);
     }
-    REQUIRE( g.score() == 20 );
+
+    SECTION("Roll all Ones") {
+        for (int i = 0; i < 20; ++i) {
+            g.roll(1);
+        }
+        REQUIRE(g.score() == 20);
+    }
 }
